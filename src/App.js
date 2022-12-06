2942716228f5da36c035d26d558001caef894e30
tangquochung <19521583@gm.uit.edu.vn>
2022-12-07 02:08:05 +0700

    Login, register

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import { Router, Routes, Route } from 'react-router-dom';
 import {Header, Body, Footer} from "./components/index"
+import Register from './page/register/register';
+import Login from './page/login/login';
 
 //App
 function App() {
-  return (
-    <>
-    <Header />
-    <Routes>
-      <Route  path='/' element={<Body />} />
-    </Routes>
-    <Footer />
-    </>
-  );
-}
+  return(
+    // <>
+    // <Header />
+    // <Routes>
+    //   <Route  path='/' element={<Body />} />
+    // </Routes>
+    // <Footer />
+    // </>
+      <Register/>);
+};
 
 export default App;
 //udate
