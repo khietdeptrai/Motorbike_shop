ed5ab38bcf0a9220ca628a9043f032474ff9a97b
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-12-12 15:59:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Header, Body, Footer } from "./components/index"
+import { Header, Home, Footer } from "./components/index"
 import Register from './page/register/register';
 import Login from './page/login/login';
 import Shopping from './page/shopping/shopping'
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate replace to="/homepage" />} />
-            <Route path='homepage' element={<Body />}></Route>
+            <Route path='homepage' element={<Home />}></Route>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/shopping' element={<Shopping />} />
