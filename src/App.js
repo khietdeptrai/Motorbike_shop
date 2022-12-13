01e8db43d8474e76fff6491b89f05fbcac8faef5
tangquochung <19521583@gm.uit.edu.vn>
2022-12-13 13:29:22 +0700

    Up cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Header, Body, Footer } from "./components/index"
+import { Header, Home, Footer } from "./components/index"
 import Register from './page/register/register';
 import Login from './page/login/login';
 import Shopping from './page/shopping/shopping'
+import Cart from './page/cart/cart';
 //App
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate replace to="/homepage" />} />
-            <Route path='homepage' element={<Body />}></Route>
+            <Route path='homepage' element={<Home />}></Route>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/shopping' element={<Shopping />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
       <Footer />
     </>
