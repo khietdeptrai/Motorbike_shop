ca58751d906ee32e55e109e6fa9520e064e38dc3
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-12-14 18:55:42 +0700

    update shopping page, home page, order card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header, Home, Footer } from "./components/index"
 import Register from './page/register/register';
 import Login from './page/login/login';
-import Shopping from './page/shopping/shopping'
+import Shopping from './page/shopping/shopping';
+import Cart from './page/cart/cart';
+import Thongtincanhan from './page/thongtincanhan/Thongtincanhan'
+import Quanlykhachhang from './page/quanlykhachhang/Quanlykhachhang';
+import Quanlysanpham from './page/quanlysanpham/Quanlysanpham';
 //App
 function App() {
   return (
@@ -14,12 +18,15 @@ function App() {
             <Route path='homepage' element={<Home />}></Route>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
         <Route path='/shopping' element={<Shopping />} />
+        <Route path='/thongtincanhan' element={<Thongtincanhan />} />
+        <Route path='/quanlykhachhang' element={<Quanlykhachhang />} />
+        <Route path='/quanlysanpham' element={<Quanlysanpham />} />
       </Routes>
       <Footer />
     </>
-
   );
 };
 
-export default App;
+export default App
--- src/page/shopping/shopping.jsx
+++ src/page/shopping/shopping.jsx
@@ -1,58 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./Shopping.scss";
+import axios from "axios";
+function Shopping() {
 
-import './Shopping.scss'
-function Cuahang() {
-    return (
-        <div className="shopping">
-            <ul className='list'>
-                <li className='item'>
-                    <div className='img-box'>
-                        <img className='img' src='https://i.pinimg.com/564x/ab/f6/93/abf6931a2219d89bce1a5ee9fb1d6daa.jpg' alt='img item' />
-                    </div>
-                    <div className='content'>
-                        <p className='name'>Name</p>
-                        <p className='price'>1000k</p>
-                        <p className='numberSale'>10 chiec</p>
-                        <button>buy now</button>
-                    </div>
-                </li>
-                <li className='item'>
-                    <div className='img-box'>
-                        <img className='img' src='https://i.pinimg.com/564x/ab/f6/93/abf6931a2219d89bce1a5ee9fb1d6daa.jpg' alt='img item' />
-                    </div>
-                    <div className='content'>
-                        <p className='name'>Name</p>
-                        <p className='price'>1000k</p>
-                        <p className='numberSale'>10 chiec</p>
-                        <button>buy now</button>
-                    </div>
-                </li>
-                <li className='item'>
-                    <div className='img-box'>
-                        <img className='img' src='https://i.pinimg.com/564x/ab/f6/93/abf6931a2219d89bce1a5ee9fb1d6daa.jpg' alt='img item' />
-                    </div>
-                    <div className='content'>
-                        <p className='name'>Name</p>
-                        <p className='price'>1000k</p>
-                        <p className='numberSale'>10 chiec</p>
-                        <button>buy now</button>
-                    </div>
-                </li>
-                <li className='item'>
-                    <div className='img-box'>
-                        <img className='img' src='https://i.pinimg.com/564x/ab/f6/93/abf6931a2219d89bce1a5ee9fb1d6daa.jpg' alt='img item' />
-                    </div>
-                    <div className='content'>
-                        <p className='name'>Name</p>
-                        <p className='price'>1000k</p>
-                        <p className='numberSale'>10 chiec</p>
-                        <button>buy now</button>
-                    </div>
-                </li>
-                
-               
-            </ul>
-        </div>
 
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://motorshop-85ou.onrender.com/api/products")
+      .then((res) => {
+        setProducts(res.data.products);
+        console.log(res.data.products);
+
+      });
+  }, []);
+
+
+
+  const handleAddtocart= (ID) => {
+    console.log(ID);
+  }
+  return (
+    <div className="shopping">
+      <ul className="list">
+        {products.map((product) => (
+          <div key={product.id}>
+            <li className="item">
+              <div className="img-box">
+                <img className="img" src={product.img} alt="img item" />
+              </div>
+              <div className="content">
+                <p className="title">{product.title}</p>
+                <p className="desc">{product.desc}</p>
+                {/* <p className="color">{product.color}</p> */}
+                <div className="color-list">
+                    <div className="item red active"></div>
+                    <div className="item yellow"></div>
+                </div>
+
+                <p className="price">{product.price} VND</p>
+                <div className="actions">
+                </div>
+                <div className="actions">
+                 <button className="btn btn-add" onClick={() => handleAddtocart(product.id)}>Add to cart</button>
+                </div>
+              </div>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default Cuahang;
+export default Shopping;
