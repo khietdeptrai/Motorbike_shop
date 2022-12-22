1c308c4dbbaf4bcc7608da34ecf5e044009d71c6
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-12-22 21:41:04 +0700

    update mod

--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -19,19 +19,19 @@ function About() {
                 </li>
                 <li className="item">
                     <img className="avatar" src="https://scontent.fsgn13-4.fna.fbcdn.net/v/t39.30808-6/279371912_1602610013447939_4309696999034125573_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ySXMtGn7_2IAX8GTjDI&tn=7pxjFtZPlA1zYO3o&_nc_ht=scontent.fsgn13-4.fna&oh=00_AfCWtKw10l1eGHCjJqK1AmS43r9h615XIxLQfI2MfczEyA&oe=639F5CBF" alt="Avatar"/>
-                    <h2 className="name">Trương Minh Khiết</h2>
+                    <h2 className="name">Tăng Quốc Hưng</h2>
                     <p className="role">Nhóm trưởng</p>
                     <p className="desc">Học, học nữa, học mãi. Học, học nữa, học mãi.Học, học nữa, học mãi Học, học nữa, học mãi.Học, học nữa, học mãi </p>
                 </li>
                 <li className="item">
                     <img className="avatar" src="https://scontent.fsgn13-4.fna.fbcdn.net/v/t39.30808-6/279371912_1602610013447939_4309696999034125573_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ySXMtGn7_2IAX8GTjDI&tn=7pxjFtZPlA1zYO3o&_nc_ht=scontent.fsgn13-4.fna&oh=00_AfCWtKw10l1eGHCjJqK1AmS43r9h615XIxLQfI2MfczEyA&oe=639F5CBF" alt="Avatar"/>
-                    <h2 className="name">Trương Minh Khiết</h2>
+                    <h2 className="name">Nguyễn Tiến Đức</h2>
                     <p className="role">Nhóm trưởng</p>
                     <p className="desc">Học, học nữa, học mãi. Học, học nữa, học mãi.Học, học nữa, học mãi Học, học nữa, học mãi.Học, học nữa, học mãi </p>
                 </li>
                 <li className="item">
                     <img className="avatar" src="https://scontent.fsgn13-4.fna.fbcdn.net/v/t39.30808-6/279371912_1602610013447939_4309696999034125573_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ySXMtGn7_2IAX8GTjDI&tn=7pxjFtZPlA1zYO3o&_nc_ht=scontent.fsgn13-4.fna&oh=00_AfCWtKw10l1eGHCjJqK1AmS43r9h615XIxLQfI2MfczEyA&oe=639F5CBF" alt="Avatar"/>
-                    <h2 className="name">Trương Minh Khiết</h2>
+                    <h2 className="name">Lê Ngọc Thái Phương</h2>
                     <p className="role">Nhóm trưởng</p>
                     <p className="desc">Học, học nữa, học mãi. Học, học nữa, học mãi.Học, học nữa, học mãi Học, học nữa, học mãi.Học, học nữa, học mãi </p>
                 </li>
