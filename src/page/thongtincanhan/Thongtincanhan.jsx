38742f1c8c6d849ce146132c0ffe089eede02142
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-12-24 14:27:28 +0700

    update

--- src/page/thongtincanhan/Thongtincanhan.jsx
+++ src/page/thongtincanhan/Thongtincanhan.jsx
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
-
+import './Thongtincanhan.scss'
+import axios from "axios";
+import ImgUser from "./icon-user.png";
 import userService from "../../service/user.service";
 
 function Thongtincanhan() {
-   
+  const [user, setUser] = useState();
+  useEffect(() => {
+    setUser(userService.getCurrentUser())
+    console.log(userService.getCurrentUser());
 
-    return ( <div>
+  }, []);
 
-    <h1>Personal Information</h1>
-
-    </div> );
-    
+  return (
+    <>
+      {user ? (
+        <div className="profile">
+          <div className="profile-left">
+            <img className="img" src={ImgUser}  alt="User Img"/>
+          </div>
+          <div className="profile-right">
+            <p className="profile-right--name">UserName: <strong>{user.username}</strong></p>
+            <p className="profile-right--text">Email: <strong>{user.email}</strong></p>
+            <p className="profile-right--text">Roles: <strong>{user.roles}</strong></p>
+            <p className="profile-right--text">Phone:{"   Đang Cập Nhật !"}</p>
+            <p className="profile-right--text">Address: {"   Đang Cập Nhật !"}</p>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default Thongtincanhan;
