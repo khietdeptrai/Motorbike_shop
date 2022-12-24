3584dcf183d6b175623ebce8c1a1bfd32c4f29d4
RouiePLNT <18521272@gm.uit.edu.vn>
2022-12-24 16:04:50 +0700

    info

--- src/page/thongtincanhan/Thongtincanhan.jsx
+++ src/page/thongtincanhan/Thongtincanhan.jsx
@@ -20,11 +20,11 @@ function Thongtincanhan() {
             <img className="img" src={ImgUser}  alt="User Img"/>
           </div>
           <div className="profile-right">
-            <p className="profile-right--name">UserName: <strong>{user.username}</strong></p>
+            <p className="profile-right--name">Username: <strong>{user.username}</strong></p>
             <p className="profile-right--text">Email: <strong>{user.email}</strong></p>
-            <p className="profile-right--text">Roles: <strong>{user.roles}</strong></p>
-            <p className="profile-right--text">Phone:{"   Đang Cập Nhật !"}</p>
-            <p className="profile-right--text">Address: {"   Đang Cập Nhật !"}</p>
+            <p className="profile-right--text">Role: <strong>{user.roles}</strong></p>
+            <p className="profile-right--text">Phone:{"   Updating !"}</p>
+            <p className="profile-right--text">Address: {"   Updating !"}</p>
           </div>
         </div>
       ) : (
